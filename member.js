7fd78ace66c9ee252b90d681df24e28a6703fab5
strawberrygarden <136162916+strawberrygarden@users.noreply.github.com>
2023-07-19 17:28:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML',
+            level: '90%'
+        },
+        {
+            name: 'CSS',
+            level: '80%'
+        },
+        {
+            name: 'Javascript',
+            level: '70%'
+        },
+        {
+            name: 'PHP',
+            level: '60%'
+        },
+        {
+            name: 'Python',
+            level: '50%'
+        }
+    ];
+    return skills;
+}
